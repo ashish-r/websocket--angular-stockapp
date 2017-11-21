e5c1f172d200d23f330e0e1b8b5928d268fcfea7
ashish-r <gettheashish@gmail.com>
2017-11-21 22:13:12 +0530

    added moment ago in time column

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var app = angular.module('myApp', []);
 
 app.controller('stockTableController', ['$scope',  function ($scope) {
   $scope.stockDataObject = {};
-  
+  var stockdata = [];
   var socket = new WebSocket('ws://stocks.mnet.website');
   
   
@@ -19,22 +19,8 @@ socket.onopen = function(event) {
 
 // Handle messages sent by the server.
 socket.onmessage = function(event) {
-  $scope.$apply(function() {
-  var stockdata = JSON.parse(event.data);
-  
-  
-    stockdata.forEach(
-      (singleStockData) => {
-
-        var rowClass = $scope.stockDataObject[singleStockData[0]] ? $scope.stockDataObject[singleStockData[0]][0] - singleStockData[1] > 0 ? "success" : "danger" : "";
-        
-          $scope.stockDataObject[singleStockData[0]] = [singleStockData[1], rowClass, (new Date()).toLocaleString()];
-          
-      }
-      );
-  
-  console.log($scope.stockDataObject);
-  });
+  stockdata = JSON.parse(event.data);
+  updateObject(1);
 };
 
 
@@ -43,6 +29,41 @@ socket.onclose = function(event) {
   console.log('Disconnected from WebSocket');
   console.log(event);
 };
+
+
+setInterval(function updateTime(){
+  updateObject(2);
+  console.log($scope.stockDataObject);
+}, 60000);
+
+function updateObject(typeop){
+  $scope.$apply(function() {
+    if(typeop === 1){
+      stockdata.forEach(
+        (singleStockData) => {
+  
+          var rowClass = $scope.stockDataObject[singleStockData[0]] ? $scope.stockDataObject[singleStockData[0]][0] - singleStockData[1] > 0 ? "success" : "danger" : "";
+          
+            $scope.stockDataObject[singleStockData[0]] = [singleStockData[1], rowClass, new Date(), timeSince(new Date())];
+            
+        }
+        );
+        stockdata = [];
+
+    }
+    else{
+      for (var key in $scope.stockDataObject) {
+        if ($scope.stockDataObject.hasOwnProperty(key)) {
+          $scope.stockDataObject[key][3] = timeSince($scope.stockDataObject[key][2]);
+        }
+     }
+
+    }
+   
+    console.log($scope.stockDataObject);
+    });
+
+}
 
   
   }]);
