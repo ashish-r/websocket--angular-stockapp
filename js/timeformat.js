4ada26fb35523c65fd635e66ff7c234558ea060d
Ashish Ranjan <gettheashish@gmail.com>
2017-11-23 15:51:10 +0530

    minor code update

--- js/timeformat.js
+++ js/timeformat.js
@@ -1,27 +1,25 @@
 // function to display time in moments form
 function timeSince(date) {
-    var seconds = Math.floor((new Date() - date) / 1000); // calculating difference of seconds between current datetime and provided date time
-    var interval = Math.floor(seconds / 31536000); //calculating no. of difference in terms of years
-    if (interval > 1) {
-        return interval + " Years Ago";
-    }
-    interval = Math.floor(seconds / 2592000); //calculating no. of difference in terms of months
-    if (interval > 1) {
-        return interval + " Months Ago";
-    }
-    interval = Math.floor(seconds / 86400); //calculating no. of difference in terms of days
-    if (interval > 1) {
-        return interval + " Days Ago";
-    }
-    interval = Math.floor(seconds / 3600); //calculating no. of difference in terms of Hours
-    if (interval > 1) {
-        return interval + " Hours Ago";
-    }
-    interval = Math.floor(seconds / 60); //calculating no. of difference in terms of Minutes
-    if (interval > 1) {
-        return interval + " Minutes Ago";
-    }
-    return " Just Now"; // For durations less than one minute
+	var seconds = Math.floor((new Date() - date) / 1000); // calculating difference of seconds between current datetime and provided date time
+	var interval = Math.floor(seconds / 31536000); //calculating no. of difference in terms of years
+	if (interval > 1) {
+		return interval + " Years Ago";
+	}
+	interval = Math.floor(seconds / 2592000); //calculating no. of difference in terms of months
+	if (interval > 1) {
+		return interval + " Months Ago";
+	}
+	interval = Math.floor(seconds / 86400); //calculating no. of difference in terms of days
+	if (interval > 1) {
+		return interval + " Days Ago";
+	}
+	interval = Math.floor(seconds / 3600); //calculating no. of difference in terms of Hours
+	if (interval > 1) {
+		return interval + " Hours Ago";
+	}
+	interval = Math.floor(seconds / 60); //calculating no. of difference in terms of Minutes
+	if (interval > 1) {
+		return interval + " Minutes Ago";
+	}
+	return " Just Now"; // For durations less than one minute
 }
-
-
